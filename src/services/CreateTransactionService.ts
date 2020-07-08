d9e2e223ff83c660847ebc7d10d784d98fd441ce
Danilo Souza <danilo_souza19@outlook.com>
2020-07-08 17:22:44 -0300

    create transaction feature

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (!(type === 'income' || type === 'outcome')) {
+      throw new AppError('Type is invalid');
+    }
+
+    const categoryRepository = getRepository(Category);
+    const transactionRepository = getRepository(Transaction);
+
+    const categoryExists = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!categoryExists) {
+      /* categoryExists = categoryRepository.create({
+        title,
+      });
+      await categoryRepository.save(categoryExists); */
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: categoryExists,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
